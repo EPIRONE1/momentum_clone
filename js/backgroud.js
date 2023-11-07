51e67150e4367d20055dfa10baf6e4197b6195d8
BuildTools <unconfigured@null.spigotmc.org>
2023-11-07 20:22:52 +0900

    v1.0

--- js/backgroud.js
+++ js/backgroud.js
@@ -2,9 +2,4 @@ const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-bgImage.id = "bgimage";
-
-document.body.append(bgImage);
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
