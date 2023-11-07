16f576ff8dd2c5ad068d1aa36fc0891e0967f34b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-07 19:54:32 +0900

    v1.0

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,7 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
-const toDoList = document.getElementById("todo-list");
+const toDoList = document.querySelector("#todo-list");
+const done = document.querySelector("#done");
 const TODOS_KEY =  "todos"
 
 let toDos = [];
@@ -10,23 +11,39 @@ function saveToDos(){
 }
 
 function deleteToDo(event){
-    const li = event.target.parentElement;
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
-    li.remove();
+    event.preventDefault();
+    const chktodo = document.querySelectorAll("input[name='todo']:checked");
+
+    chktodo.forEach((todo) => {
+        const li = todo.parentElement;
+        console.log(li);
+        toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+        li.remove();
+    });
     saveToDos();
 }
 
 function paintToDo(newTodo){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newTodo.text;
-    li.id = newTodo.id;
-    const button = document.createElement("button");
-    button.innerText="X";
-    button.addEventListener("click",deleteToDo)
-    li.appendChild(span);
-    li.appendChild(button);
-    toDoList.appendChild(li);
+    const chktodo = document.querySelectorAll("input[name='todo']");
+    if(chktodo.length <= 10) {
+        const li = document.createElement("li");
+        li.id = newTodo.id;
+
+        const label = document.createElement("label");
+        label.innerText = newTodo.text;
+        label.htmlFor = `chk${newTodo.id}`;
+
+        const input = document.createElement("input");
+        input.type = "checkbox";
+        input.id = newTodo.id;
+        input.name = "todo";
+
+        li.appendChild(input);
+        li.appendChild(label);
+        toDoList.appendChild(li);
+    }
+
+   
 }
 
 function handleToDoSubmit(event){
@@ -42,6 +59,7 @@ function handleToDoSubmit(event){
     saveToDos();
 }
 
+done.addEventListener("click",deleteToDo);
 toDoForm.addEventListener("submit",handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
